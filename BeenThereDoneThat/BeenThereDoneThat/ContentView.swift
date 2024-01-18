31c06d57d4a15a26b5d2efec4300cac7a3a82e63
christina choi <cchoi17@lion.lmu.edu>
2024-01-17 16:40:04 -0800

    bare bones of app

---
+++ BeenThereDoneThat/BeenThereDoneThat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeenThereDoneThat
+//
+//  Created by Christina Choi on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("meow!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
