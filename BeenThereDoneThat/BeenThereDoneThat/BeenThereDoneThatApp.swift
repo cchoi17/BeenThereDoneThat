cc6b0edaa73f6ba15b3db0e82816c963b230a762
christina choi <cchoi17@lion.lmu.edu>
2024-01-24 14:34:10 -0800

    new

---
+++ BeenThereDoneThat/BeenThereDoneThat/BeenThereDoneThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeenThereDoneThatApp.swift
+//  BeenThereDoneThat
+//
+//  Created by Christina Choi on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BeenThereDoneThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
