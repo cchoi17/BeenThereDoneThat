db6384ea21a015d2bb9de8670439a29973531017
christina choi <cchoi17@lion.lmu.edu>
2024-01-30 13:26:49 -0800

    Working on Authentication Page

--- BeenThereDoneThat/BeenThereDoneThat/LoginScreen/LoginView.swift
+++ BeenThereDoneThat/BeenThereDoneThat/LoginScreen/LoginView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct LoginView: View {
-    let pink = Color(red: 0.87, green: 0.52, blue: 0.73, opacity: 0.9)
-    let green = Color(red: 0.039,green: 0.49, blue: 0.298, opacity:0.9)
-    let beige = Color(red: 0.81, green: 0.73, blue: 0.59, opacity:0.7)
+
     let image1 = Image("loginPic")
     let image2 = Image("checkers")
     var body: some View {
@@ -22,14 +20,14 @@ struct LoginView: View {
                 .font(.custom("Cooper Lt BT", size:30))
                 .padding(.top,50)
             NavigationLink{
-                Text("Hello") // replace this with a login view
+                AuthenticationView()
             }label: {
                 Text("Login")
                     .font(.custom("Cooper Lt BT", size:20))
                     .foregroundColor(.white)
                     .frame(height:55)
                     .frame(width: 300)
-                    .background(pink)
+                    .background(Color("MyGreen"))
                     .cornerRadius(10)
                     .padding(.top,20)
             }
@@ -41,7 +39,7 @@ struct LoginView: View {
                     .foregroundColor(.white)
                     .frame(height:55)
                     .frame(width: 300)
-                    .background(green)
+                    .background(Color("MyPink"))
                     .cornerRadius(10)
             }
             image1.resizable()
@@ -51,9 +49,11 @@ struct LoginView: View {
                 .padding(.top,111.0)
                 .frame(width: 30.0, height:0)
         }
+        .navigationTitle("")
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(beige)
+        .background(Color("MyBeige"))
     }
+        
 }
 
 struct LoginView_Previews: PreviewProvider{
